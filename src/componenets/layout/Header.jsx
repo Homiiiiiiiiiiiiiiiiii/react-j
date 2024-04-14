45ca23d7355eede42cd0c47e1c0dde63460efe7a
homi <entekk1112@naver.com>
2024-04-14 09:38:07 +0900

    now

--- src/componenets/layout/Header.jsx
+++ src/componenets/layout/Header.jsx
@@ -1,15 +1,17 @@
 import { Link, useLocation } from "react-router-dom"
 import Button, { GhostButton } from "../button/Button"
+import tw from "twin.macro"
+import styled from "styled-components"
 
 function Header(){
 
     const location = useLocation() //리액트에서 제공하는 현재위치
 
     return(
-        <header className="flex  justify-between items-center bg-gray-200 p-5 ">
-            <h1 className="font-bold text-3xl">logo</h1>
-            <nav>
-                <ul className="flex gap-6 text-xl">
+        <header className="flex justify-between items-center bg-gray-300 p-5 ">
+            <h1 className="font-bold text-3xl text-secondary">logo</h1>
+            <Gnb>
+                <ul>
                     <li className={location.pathname === '/' ? 'active' : ''}>
                         <Link to='/'>Home</Link>    
                     </li>
@@ -20,7 +22,7 @@ function Header(){
                         <Link to='/login'>Login</Link>    
                     </li>
                 </ul>
-            </nav>
+            </Gnb>
 
             <div>
                 <Button default>마이페이지</Button>
@@ -30,4 +32,17 @@ function Header(){
     )
 }
 
+// const Gnb = tw.nav`
+//     bg-secondary p-3
+// `
+const Gnb = styled.nav`
+    ul{
+        ${tw`flex gap-6`}
+    }
+    li{
+        a{
+            ${tw`text-white`}
+        }
+    }
+`
 export default Header
